fa9ea559947d2e2afe382534098ef6af0aaca577
Florian Raith <37345813+FlorianRaith@users.noreply.github.com>
2023-06-16 15:40:29 +0200

    Enable cors only for development (#8)

--- src/channel/channel.gateway.ts
+++ src/channel/channel.gateway.ts
@@ -10,12 +10,14 @@ import { MikroORM, UseRequestContext } from '@mikro-orm/core';
 import { Server, Socket } from 'socket.io';
 import { ChannelService } from './channel.service';
 import { Channel } from './channel';
+import * as process from 'process';
 
-@WebSocketGateway({
-  cors: {
-    origin: process.env.FRONTEND_URL,
-  },
-})
+const WEB_SOCKET_OPTIONS =
+  process.env.NODE_ENV === 'production'
+    ? {}
+    : { cors: { origin: process.env.FRONTEND_URL } };
+
+@WebSocketGateway(WEB_SOCKET_OPTIONS)
 export class ChannelGateway implements OnGatewayConnection {
   @WebSocketServer()
   public server: Server;
