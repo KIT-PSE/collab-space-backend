de5d99346da1f8bd24d4a57fa470b92d29602faf
Florian Raith <37345813+FlorianRaith@users.noreply.github.com>
2023-06-29 15:03:24 +0200

    Fix env bug (#11)

--- src/channel/channel.gateway.ts
+++ src/channel/channel.gateway.ts
@@ -10,7 +10,9 @@ import { MikroORM, UseRequestContext } from '@mikro-orm/core';
 import { Server, Socket } from 'socket.io';
 import { ChannelService } from './channel.service';
 import { Channel } from './channel';
-import * as process from 'process';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const WEB_SOCKET_OPTIONS =
   process.env.NODE_ENV === 'production'
