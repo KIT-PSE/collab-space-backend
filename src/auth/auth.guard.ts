d335f0ca87dbf8280e80263db54035560a4cb53c
Marius Friess <friess.marius@gmail.com>
2023-07-09 20:13:13 +0200

    refresh jwt inside of AuthGuard requests

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,5 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthGuard extends PassportAuthGuard('jwt') {}
+export class AuthGuard extends PassportAuthGuard('jwt') {
+  constructor(
+    private readonly auth: AuthService,
+    private readonly jwtService: JwtService,
+  ) {
+    super();
+  }
+  async canActivate(context) {
+    const _canActivate = (await super.canActivate(context)) as boolean;
+
+    if (_canActivate) {
+      const request = context.switchToHttp().getRequest();
+      const response = context.switchToHttp().getResponse();
+
+      const user = await this.auth.user();
+
+      const payload = { sub: user.id };
+      const token = this.jwtService.sign(payload);
+      response.cookie('jwt', token, { httpOnly: true });
+    }
+
+    return _canActivate;
+  }
+}
