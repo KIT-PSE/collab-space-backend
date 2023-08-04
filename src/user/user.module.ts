91dec6994f7293eae051e105ba6962aa124564d0
Negin Moshki <133236446+NeginMoshki@users.noreply.github.com>
2023-08-04 16:25:26 +0200

    DocUpdate user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,16 @@
+/**
+ * @fileOverview UserModule definition.
+ */
+
 import { Global, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { User } from './user.entity';
 import { UserController } from './user.controller';
 
+/**
+ * Global module providing user-related functionality.
+ */
 @Global()
 @Module({
   imports: [MikroOrmModule.forFeature([User])],
